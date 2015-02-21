ffb81925ef1a7c6a2b5e52f488b0793e34c73a2a
TalKashi <shakikashi@gmail.com>
2015-02-21 14:48:56 +0200

    ListView

--- app/src/main/java/com/lucid/blockit/blockit/MainActivity.java
+++ app/src/main/java/com/lucid/blockit/blockit/MainActivity.java
@@ -1,39 +1,18 @@
 package com.lucid.blockit.blockit;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        String values[] = {"052-6290696", "054-7836876", "09-7413090"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_main, values);
+        setListAdapter(adapter);
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
