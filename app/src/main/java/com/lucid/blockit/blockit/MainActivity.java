67c1487d1c23277477653eed1c0bd0cb0196a051
SergeOxe <sergeoxe@gmail.com>
2015-02-21 15:44:04 +0200

    PLZ WORK

--- app/src/main/java/com/lucid/blockit/blockit/MainActivity.java
+++ app/src/main/java/com/lucid/blockit/blockit/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String values[] = {"052-6290696", "054-7836876", "09-7413090"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.activity_main, values);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.row_layout, R.id.phoneNumber, values);
         setListAdapter(adapter);
 
     }
